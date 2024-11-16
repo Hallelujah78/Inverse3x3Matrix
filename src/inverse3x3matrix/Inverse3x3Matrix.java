0d5c984ed0a78313098888e33cf83b04ee54f559
Gavan Browne <gavan.browne@gmail.com>
2024-11-16 19:32:06 +0000

    add method to convert array index to row and col

--- src/inverse3x3matrix/Inverse3x3Matrix.java
+++ src/inverse3x3matrix/Inverse3x3Matrix.java
@@ -5,6 +5,7 @@
 package inverse3x3matrix;
 
 import java.util.Arrays;
+import java.util.HashMap;
 
 /**
  *
@@ -33,6 +34,16 @@ public class Inverse3x3Matrix {
         return transposedMatrix;
     }
 
+    // a method that takes the index of an array and a matrix side length and
+    //converts the the index to a row and column value in the matrix
+    private static HashMap convert1DTo2D(int index, int sideLength) {
+        HashMap<String, Integer> rowAndCol = new HashMap<>();
+
+        rowAndCol.put("row", Math.floorDiv(index, sideLength));
+        rowAndCol.put("col", index % sideLength);
+        return rowAndCol;
+    }
+
     private static int calc2x2Determinant(int[][] matrix) {
         return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0];
     }
@@ -41,6 +52,7 @@ public class Inverse3x3Matrix {
         int numElements = matrix.length * matrix[0].length; // 9 elements in the matrix
         int sideLength = matrix.length; // 3x3 matrix
         int determinant = 0; // store the determinant sum as we calculate it
+        int sideLength2x2 = 2;
 
         // for all elements in first row of matrix
         for (int i = 0; i < sideLength; i++) {
@@ -49,12 +61,12 @@ public class Inverse3x3Matrix {
             If we flatten the matrix into a 1D array, then we can calculate the
             row and column of the i-th element of the 1D array
              */
-            int row = Math.floorDiv(i, sideLength);
-            int col = i % sideLength;
-
+            // int row = Math.floorDiv(i, sideLength);
+            // int col = i % sideLength;
+            HashMap<String, Integer> rowAndCol = convert1DTo2D(i, sideLength);
             // we get the scalar in our original array using the row and col we
             // calculated
-            int scalar = matrix[row][col];
+            int scalar = matrix[rowAndCol.get("row")][rowAndCol.get("col")];
 
             /* we use minorIndex for the following:
             i) To keep track of our position in the matrix of minors as we fill
@@ -62,7 +74,7 @@ public class Inverse3x3Matrix {
             ii) To terminate our inner for loop once minorMatrix is full
              */
             int minorIndex = 0;
-            int minorMatrix[][] = new int[2][2];
+            int minorMatrix[][] = new int[sideLength2x2][sideLength2x2];
 
             // this loop constructs the matrix of minors for each element a i
             for (int j = 0; j < numElements; j++) {
@@ -81,7 +93,7 @@ public class Inverse3x3Matrix {
                 int currCol = j % sideLength;
 
                 // ignore the row and column that contain our element at index i
-                if (currRow == row || currCol == col) {
+                if (currRow == rowAndCol.get("row") || currCol == rowAndCol.get("col")) {
                     continue;
                 }
 
@@ -109,7 +121,7 @@ public class Inverse3x3Matrix {
 
     }
 
-    private static void constructCofactorMatrix(int[][] matrix) {
+    private static int[][] constructCofactorMatrix(int[][] matrix) {
         int numElements = matrix.length * matrix[0].length; // 9 elements in the matrix
         int sideLength = matrix.length; // 3x3 matrix
         int determinant = 0; // store the determinant sum as we calculate it
@@ -192,13 +204,26 @@ public class Inverse3x3Matrix {
 
             cofactor3x3MatrixIndex++;
         }
-        System.out.println("determinant of 3x3 matrix: " + Arrays.deepToString(cofactor3x3Matrix));
 
-        // transpose the matrix of cofactors:
-        int[][] matrixTransposed = transposeMatrix(cofactor3x3Matrix);
+        return cofactor3x3Matrix;
     }
 
-    private static int[][] constructInverseMatrix() {
+    private static double[][] constructInverseMatrix(int determinant, int[][] matrix) {
+
+        int numRows = matrix.length;
+        int numCols = matrix[0].length;
+
+        double[][] inverseMatrix = new double[numRows][numCols];
+        // for every row of matrix
+        for (int i = 0; i < numRows; i++) {
+
+            // for every column
+            for (int j = 0; j < numCols; j++) {
+                inverseMatrix[i][j] = (matrix[i][j] * 1.0 / determinant);
+            }
+        }
+        System.out.println("inverse matrix: " + Arrays.deepToString(inverseMatrix));
+        return inverseMatrix;
     }
 
     /**
@@ -214,8 +239,12 @@ public class Inverse3x3Matrix {
 
         System.out.println("3x3 det: " + determinant);
 
-        // test constructing minor matrix
-        constructCofactorMatrix(anotherMatrix);
+        // construct cofactor matrix
+        int[][] cofactorMatrix = constructCofactorMatrix(anotherMatrix);
+        // transpose the matrix of cofactors:
+        int[][] adjunctMatrix = transposeMatrix(cofactorMatrix);
+        // construct inverse matrix
+        constructInverseMatrix(determinant, adjunctMatrix);
     }
 
 }
